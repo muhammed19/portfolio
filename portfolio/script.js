98de16ea1a8af813f50ef1ac8431cd28e9afb9a4
muhammed19 <mdshameemhossain26@gmail.com>
2025-06-16 08:00:42 +0600

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,5 @@
+// Define a function named showMessage
+function showMessage() {
+  // Show a popup alert message when the function is called
+  alert("Thanks for visiting Shameem's portfolio!");
+}
